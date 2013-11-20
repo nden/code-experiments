09aa744aab472ef2398f0a0ec58a8725bf957a16
Nadia Dencheva <nadia.astropy@gmail.com>
2013-11-20 14:04:06 -0500

    remove references to pyfits, add properties, fix coordinate transformations

--- generalized_wcs_api/prototype/coordinate_systems.py
+++ generalized_wcs_api/prototype/coordinate_systems.py
@@ -4,7 +4,7 @@
 from astropy import time
 from astropy import coordinates as coo
 from astropy import units as u
-
+from astropy import utils as astu
 
 class CoordinateFrame(object):
     """
@@ -20,16 +20,32 @@ class CoordinateFrame(object):
     """
     def __init__(self, num_axes, ref_system=None, ref_pos=None, units=None, axes_names=None):
         """ Initialize a frame"""
-        if units is not None:
-            if len([units]) != num_axes:
+        if units is not None and astu.isiterable(units):
+            if len(units) != num_axes:
                 raise ValueError("Number of units does not match number of axes")
         self._units=units
-        if axes_names is not None:
-            if len([axes_]) != num_axes:
+        if axes_names is not None and astu.isiterable(axes_names):
+            if len(axes_names) != num_axes:
                 raise ValueError("Number of axes names does not match number of axes")
         self._axes_names = axes_names
         self._reference_system = ref_system
-        delf._reference_position = ref_pos
+        self._reference_position = ref_pos
+        
+    @property
+    def units(self):
+        return self._units
+
+    @units.setter
+    def units(self, val):
+        self._units = val
+
+    @property
+    def axes_names(self):
+        return self._axes_names
+
+    @units.setter
+    def axes_names(self, val):
+        self._axes_names = val
 
     def transform_to(self, other):
         """
@@ -104,21 +120,29 @@ class SkyFrame(CoordinateFrame):
         self._projection = projection
         self._obstime = obstime
         self._offset_center = offset_center
+        
+    @property
+    def obstime(self):
+        return self._obstime
+
+    @property
+    def equinox(self):
+        return self._equinox
 
     def transform_to(self, lat, lon, other):
         """
         Transform from the current reference system to other.
         """
-        func = self._wrap_func(self, lat, lon, other)
+        func = self._wrap_func(lat, lon, other)
         return func(lat, lon)
 
     def _wrap_func(self, lat, lon, other):
         def create_coordinates(lat, lon):
-            coord_klass = getattr(coo, self.reference_system+"Coordinates")
-            current_coordinates = coord_klass(lat, lon, unit=self.unit,
+            coord_klass = getattr(coo, self._reference_system)
+            current_coordinates = coord_klass(lat, lon, unit=self.units,
                                               obstime=self.obstime, equinox=self.equinox)
             return current_coordinates.transform_to(other)
-
+        return create_coordinates
 
 class SpectralFrame(CoordinateFrame):
     """
@@ -142,23 +166,25 @@ class SpectralFrame(CoordinateFrame):
     spectral_ref_frame = ["WAVE", "FREQ", "ENER", "WAVEN",  "AWAV", "VRAD",
                           "VOPT", "ZOPT", "VELO", "BETA"]
 
-    standard_reference_positions = ["GEOCENTER", "BARYCENTER", "HELIOCENTER",
+    standard_reference_position = ["GEOCENTER", "BARYCENTER", "HELIOCENTER",
                                     "TOPOCENTER", "LSR", "LSRK", "LSRD",
                                     "GALACTIC_CENTER", "MOON", "LOCAL_GROUP_CENTER",
                                     "EMBARYCENTER", "RELOCATABLE", "UNKNOWNRefPos"]
 
     def __init__(self, reference_system, reference_position, dateobs, units, axes_names=None,
                  rest_freq=None, rest_wave=None):
+
         if axes_names is None:
             axes_names = reference_system
         super(SpectralFrame, self).__init__(num_axes=1, ref_system=reference_system, ref_pos=reference_position,
                                             axes_names=axes_names, units=units)
+        self._validate_reference_position()
         self._rest_freq = rest_freq
         self._rest_wave = rest_wave
         self._dateobs = dateobs
 
     def transfer_to(self, coordinate, other):
-        u.
+        return u.Quantity(coordinate).to(other.units, equivalencies=u.spectral())
 
 class CoordinateSystem(object):
     """
